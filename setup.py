0e5c28fbcc39d4377eb0b03e170afdd3fca50597
g-tierney <gtierney2@gmail.com>
2021-04-25 01:04:52 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 17:27:51 2021
+
+@author: grahamtierney
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fifa-gp", 
+    version="0.0.5",
+    author="Alessandro Zito, Graham Tierney, and Vittorio Orlandi",
+    author_email="vittorio.orlandi@duke.edu",
+    description="Python implementation of FIFA-GP from Moran and Wheeler (2020)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vittorioorlandi/STA663_FIFA_GP",
+    project_urls={
+        "Bug Tracker": "https://github.com/vittorioorlandi/STA663_FIFA_GP/issues",
+    },
+    license = "LICENSE.txt",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["fifa_gp"],
+    python_requires=">=3.6",
+)
